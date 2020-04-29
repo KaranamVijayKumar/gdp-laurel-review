98d4e33cf1edd8bedcc100f9e18559dcb68cb321
Manasa Goriparthi <manasaprasad97@gmail.com>
2020-04-29 14:43:53 -0500

    database uploaded

---
+++ Code/Source Code/app/Project/Commands/migrate.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * File: migrate.php
+ * Created: 27-07-2014
+ *
+ * Description
+ *
+ * @author   Arpad Olasz <arpi@wingsline.com>
+ */
+
+
+// Start database connection
+$db = new \Story\DB(config('database', null));
+
+// Connect to databse server
+$db->connect();
+
+
+// Create migration object
+$migrationName = '\Story\Migration\\' . studly($db->pdo->getAttribute(PDO::ATTR_DRIVER_NAME));
+$migration = new $migrationName;
+
+// Set database connection
+$migration->db = $db;
+
+// Load table configuration
+$migration->tables = require(SP . 'config/migrations.php');
+
+// create the schemas
+$migration->createSchema();
