d2bc52837eaabda8a94361fba81d620d97ddb827
harsha4824 <46695083+harsha4824@users.noreply.github.com>
2020-04-30 01:27:39 -0500

    Preview

---
+++ Code/Source Code/app/Project/Services/Preview.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * File: Preview.php
+ * Created: 08-09-2014
+ *
+ * Description
+ *
+ * @author   Arpad Olasz <arpi@wingsline.com>
+ */
+
+namespace Project\Services;
+
+abstract class Preview implements PreviewInterface
+{
+
+}
