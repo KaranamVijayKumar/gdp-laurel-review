0667806eb1a571141f0aa8cadad74c42f621d9c2
harsha4824 <46695083+harsha4824@users.noreply.github.com>
2020-04-30 01:28:12 -0500

    PreviewInterface

---
+++ Code/Source Code/app/Project/Services/PreviewInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * File: PreviewInterface.php
+ * Created: 08-09-2014
+ *
+ * Description
+ *
+ * @author   Arpad Olasz <arpi@wingsline.com>
+ */
+
+namespace Project\Services;
+
+interface PreviewInterface
+{
+}
