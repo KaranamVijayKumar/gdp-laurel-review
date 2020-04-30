037988136f26f040944f27325a1b403db6cc2d92
harsha4824 <46695083+harsha4824@users.noreply.github.com>
2020-04-30 01:08:23 -0500

    BoxPreviewException

---
+++ Code/Source Code/app/Project/Services/Box/BoxPreviewException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * File: BoxPreviewException.php
+ * Created: 13-09-2014
+ *
+ * Description
+ *
+ * @author   Arpad Olasz <arpi@wingsline.com>
+ */
+
+namespace Project\Services\Box;
+
+class BoxPreviewException extends \Exception
+{
+}
