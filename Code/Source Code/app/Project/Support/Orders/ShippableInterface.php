b6e7a3aec4ff347e80b72a36c314aa976526f387
Ravi Kumar Atluri <ravikumar.atluri9@gmail.com>
2020-05-01 20:41:44 -0500

    ShippableInterface.php commit

---
+++ Code/Source Code/app/Project/Support/Orders/ShippableInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * File: ShippableInterface.php
+ * Created: 06-05-2015
+ *
+ * Description
+ *
+ * @author   Arpad Olasz <arpi@wingsline.com>
+ */
+
+namespace Project\Support\Orders;
+
+/**
+ * Interface ShippableInterface
+ * @package Project\Support\Orders
+ */
+interface ShippableInterface
+{
+}
